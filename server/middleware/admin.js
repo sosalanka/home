7c15c05ad3c35856638d4477827aa97df725da19
sosalanka <sosalanka@gmail.com>
2024-12-30 01:17:18 +0530

    Delete server directory

--- server/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-// middleware/admin.js
-const admin = async (req, res, next) => {
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ message: 'Access denied' });
-    }
-    next();
-};
-
-module.exports = { auth, admin };
