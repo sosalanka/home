04514d4f1fbc6f2f503adbabedbe40c4a9e02cb4
sosalanka <sosalanka@gmail.com>
2024-12-29 14:59:19 +0530

    Add files via upload

---
+++ server/middleware/admin.js
@@ -0,0 +1,9 @@
+// middleware/admin.js
+const admin = async (req, res, next) => {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+};
+
+module.exports = { auth, admin };
